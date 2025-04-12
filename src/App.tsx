c39ccdbbafed367173aa7870a0971bd6c4725512
santanaIgot <santanaigoor01@gmail.com>
2025-04-12 19:59:42 -0300

    [feat] - rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { Button } from "./components/button";
 import { ThemeProvider } from "styled-components";
-import { defaultTheme } from './styles/themes/default'; // CORRETO!
+import { defaultTheme } from "./styles/themes/default"; // CORRETO!
 import { GlobalStyle } from "./styles/global";
+
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Button color="primary" />
-      <Button color="danger" />
-      <Button color="success" />
-      <Button />
-      <GlobalStyle/>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        
+        <Router />
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
