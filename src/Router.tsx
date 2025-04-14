8e02c022a6cbdac77cd8e771cd8baea9ba80d549
santanaIgot <santanaigoor01@gmail.com>
2025-04-14 18:24:40 -0300

    [fix] - add route outlet

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,15 @@
 import {Routes, Route} from 'react-router-dom'
 import { Home } from './pages/home'
 import { History } from './pages/History' 
+import { Index } from './layout/DefaultLayout'
 
 export function Router() {
     return(
         <Routes>
+           <Route path='/' element={<Index/>}>
             <Route path='/' element={<Home/>}/>
             <Route path='/history' element={<History/>}/>
+           </Route>  
         </Routes>
     )
 }
