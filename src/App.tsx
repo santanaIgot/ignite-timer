2b59819a50423c58177bc3dc5f9a27a740c52125
santanaIgot <santanaigoor01@gmail.com>
2025-04-14 18:24:11 -0300

    [fix] - fix app

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import { GlobalStyle } from "./styles/global";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Router";
 
+
 export function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={defaultTheme}>
-        
         <Router />
         <GlobalStyle />
       </ThemeProvider>
