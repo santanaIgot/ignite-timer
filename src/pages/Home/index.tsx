8d2ee6845f7b8033fa8da18bf9b6908a7c366a2b
santanaIgot <santanaigoor01@gmail.com>
2025-04-15 22:55:00 -0300

    [feat] - estilização botão e input

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { Play } from "phosphor-react";
-import { CountDownContainer, FormContainer, HomeContainer, Separetor } from "./styles";
+import { CountDownContainer, FormContainer, HomeContainer, Separetor, StartCountDownButton, TaskInput, MinutesAmountInput } from "./styles";
 
 
 export function Home() {
@@ -8,10 +8,17 @@ export function Home() {
       <form action="">
         <FormContainer >
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
+        <TaskInput 
+          id="task" 
+          placeholder="Dê um nome para o projeto"
+        />
 
           <label htmlFor="">durante </label>
-          <input type="number" id="minutesAmount" />
+        <MinutesAmountInput 
+          type="number" 
+          id="minutesAmount" 
+          placeholder="00"
+        />
 
           <span>minutos.</span>
         </FormContainer>
@@ -24,7 +31,9 @@ export function Home() {
           <span>0</span>
         </CountDownContainer>
 
-        <button type="submit">Começar <Play size={24}/></button>
+        <StartCountDownButton disabled type="submit">
+          Começar <Play size={24}/>
+        </StartCountDownButton>
       </form>
     </HomeContainer>
   );
