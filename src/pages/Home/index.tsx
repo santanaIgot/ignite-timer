21b90557ba5d79e819ea7f6f31ad7b5ebda2718e
santanaIgot <santanaigoor01@gmail.com>
2025-04-15 23:36:29 -0300

    [feat] - input proproties step and datalist in input

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,13 +11,24 @@ export function Home() {
         <TaskInput 
           id="task" 
           placeholder="Dê um nome para o projeto"
+          list="task-suggestions"
         />
+
+        <datalist id="task-suggestions">
+          <option value="Projeto 1"></option>
+          <option value="Projeto 2"></option>
+          <option value="Projeto 3"></option>
+          <option value="Projeto 4"></option>
+        </datalist>
 
           <label htmlFor="">durante </label>
         <MinutesAmountInput 
           type="number" 
           id="minutesAmount" 
           placeholder="00"
+          step={5}
+          min={5}
+          max={60}
         />
 
           <span>minutos.</span>
