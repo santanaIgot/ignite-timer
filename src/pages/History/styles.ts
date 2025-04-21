8f0eb6a9672f57aaf863507ecce39db8f32fbe47
santanaIgot <santanaigoor01@gmail.com>
2025-04-20 23:03:04 -0300

    [feat] - new style to the page history

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -1,29 +1,106 @@
 import styled from "styled-components";
 
-export const HistoryContainer = styled.div`
-    /* background: ${props => props.theme["gray-500"]}; */
-`
-
-
-export const TarefaContainer = styled.div`
-    background:  ${props => props.theme["gray-500"]};
-    margin-top: 1rem;
-    height: 2rem;
-    padding: 2rem;
-    align-items: flex-start;
-    text-align: center;
-    justify-content: center;
-    border-radius: 5px;
-`
-
-export const ListContainer = styled.li`
+export const HistoryContainer = styled.main`
+    flex: 1;
+    padding: 3.5rem;
+    
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-    
-    ul{
-        margin-top: -0.5rem;
-    }   
+    flex-direction: column;
+    height: 100%;
+
+    h1{
+        font-size: 1.5rem;
+        color: ${props => props.theme["gray-100"]};
+    }
+`
+
+
+
+export const ListContainer = styled.div`
+   flex: 1;
+   overflow: auto;
+   margin-top: 2rem;
    
-    
+   table{
+    border-collapse: collapse;
+    width: 100%;
+    min-width: 600px;
+   }
+
+   th{
+    background-color: ${props=> props.theme["gray-600"]};
+    line-height: 1.6;
+    text-align: left;
+    padding: 1rem;
+    font-size: 0.876rem;
+    color:${props => props.theme["gray-100"]};
+
+    &:first-child{
+        border-top-left-radius: 8px ;
+        padding-left: 1.5rem;
+    }
+    &:last-child{
+        border-top-right-radius: 8px ;
+        padding-right: 1.5rem;
+    }
+
+   }
+
+
+
+
+   td{
+    background-color: ${(props) => props.theme["gray-700"]};
+    border-top: 4px solid ${(props) => props.theme["gray-800"]};
+    padding: 1rem;
+    font-size: 0.875rem;
+    line-height: 1.6;
+    justify-content: space-between;
+
+    &:first-child{
+        width: 50%;
+        padding-left: 1.5rem;
+        }
+    &:last-child{
+        
+        padding-right: 1.5rem;
+    }
+   }
+
+
+
+
+
+
+
+`
+
+
+const STATUS_COLORS = {
+    yellow: 'yellow-500',
+    green: 'green-500',
+    red:'red-500',
+
+} as const 
+
+
+interface StatusProps {
+    statusColor: keyof typeof STATUS_COLORS;
+}
+
+
+
+
+export const Status = styled.span<StatusProps>`
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    &::before{
+        content: '';
+        width: 0.5rem;
+        height: 0.5rem;
+        border-radius: 50%;
+        background: ${(props)=> props.theme[STATUS_COLORS[props.statusColor]]};
+    }
 `
